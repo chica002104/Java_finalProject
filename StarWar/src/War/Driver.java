637f110685a0c8bfebb25cc30b4c9069c5e32c62
Hung_MH <32357127+chica002104@users.noreply.github.com>
2018-12-28 12:57:40 +0800

    1

--- StarWar/src/War/Driver.java
+++ StarWar/src/War/Driver.java
@@ -2,12 +2,12 @@ package War;
 
 import javax.swing.JFrame;
 
-
+import Tank.GamePanel;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Star War");
+		JFrame frame = new JFrame("Unit2, Lab20");
 		//frame.setSize(800, 800);
 		frame.setSize(1440, 900);
 		frame.setLocation(0, 0);
